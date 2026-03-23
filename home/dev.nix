049b917a7c9f3fcbe54b419bcec7c1ce81c7bfd4
eshrh <esrh@gatech.edu>
2026-03-23 15:49:42 +0900

    update dev packages

--- home/dev.nix
+++ home/dev.nix
@@ -3,22 +3,14 @@
     (pkgs.python3.withPackages (python-pkgs: [
       python-pkgs.pandas
       python-pkgs.numpy
-      python-pkgs.scipy
-      python-pkgs.scikit-learn
       python-pkgs.matplotlib
-      python-pkgs.seaborn
       python-pkgs.requests
       python-pkgs.ipython
-      python-pkgs.pyarrow
       python-pkgs.pyqt6
-      python-pkgs.pyzmq
 
       python-pkgs.python-lsp-server
       python-pkgs.pylsp-mypy
       python-pkgs.pylsp-rope
-
-      python-pkgs.jax
-      python-pkgs.jaxlib
     ]))
     pkgs.gnumake
     pkgs.gcc
