0b4de58c6c3e62cde414b813a44adffabf31ddb7
eshrh <esrh@esrh.me>
2026-06-12 19:04:46 +0900

    add emacs config tangle step to nix build

--- home/emacs/emacs.nix
+++ home/emacs/emacs.nix
@@ -2,7 +2,17 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  emacs = config.programs.emacs.finalPackage;
+  # Tangle init.org -> init.el at build time using org-babel-tangle.
+  tangledInit = pkgs.runCommand "init.el" {} ''
+    cp ${./conf/init.org} init.org
+    ${emacs}/bin/emacs --batch \
+      --eval "(require 'org)" \
+      --eval '(org-babel-tangle-file "init.org")'
+    cp init.el $out
+  '';
+in {
   programs.emacs.enable = true;
   home.sessionVariables = {
     EDITOR = "emacs";
@@ -11,4 +21,5 @@
     source = ./conf;
     recursive = true;
   };
+  xdg.configFile."emacs/init.el".source = tangledInit;
 }
