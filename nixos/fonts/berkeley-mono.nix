c9a92f6381c07c6308978e669bf7b4d5d4e2dd82
eshrh <esrh@esrh.me>
2026-08-13 12:11:27 -0400

    font

--- nixos/fonts/berkeley-mono.nix
+++ nixos/fonts/berkeley-mono.nix
@@ -6,13 +6,7 @@ pkgs.stdenv.mkDerivation {
   src = pkgs.requireFile {
     name = "tx02.tar.gz";
     hash = "sha256-7tuSs/rmUjCeetWMvVBnVMgpjJ/mQYsdvzUtSPB2S6k=";
-    message = ''
-      Berkeley Mono must be obtained separately.
-
-      Add the licensed archive to the Nix store with:
-
-        nix-store --add-fixed sha256 /path/to/tx02.tar.gz
-    '';
+    message = "nix-store --add-fixed sha256 /path/to/tx02.tar.gz";
   };
 
   unpackPhase = ''
