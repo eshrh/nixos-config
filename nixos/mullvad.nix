fd236e5e6af0d827fa7350a48a8effc9a3344ba5
eshrh <esrh@esrh.me>
2026-09-04 22:19:12 -0400

    make 'mullvad lan set allow' declarative

--- nixos/mullvad.nix
+++ nixos/mullvad.nix
@@ -1,4 +1,27 @@
-{pkgs, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   environment.systemPackages = [pkgs.mullvad-vpn pkgs.mullvad];
   services.mullvad-vpn.enable = true;
+
+  systemd.services.mullvad-allow-lan = {
+    description = "Allow LAN traffic through Mullvad";
+    wantedBy = ["multi-user.target"];
+    requires = ["mullvad-daemon.service"];
+    after = ["mullvad-daemon.service"];
+    partOf = ["mullvad-daemon.service"];
+    startLimitBurst = 30;
+    startLimitIntervalSec = 60;
+
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${lib.getExe config.services.mullvad-vpn.package} lan set allow";
+      RemainAfterExit = true;
+      Restart = "on-failure";
+      RestartSec = 1;
+    };
+  };
 }
