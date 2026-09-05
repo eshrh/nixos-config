acf449f0472599f02e2cf2d14dce0be8bdb5b4ab
eshrh <esrh@esrh.me>
2026-09-05 16:13:40 -0400

    magnolia ssh/cloudflared

--- hosts/magnolia/configuration.nix
+++ hosts/magnolia/configuration.nix
@@ -11,6 +11,37 @@
 
   services.fstrim.enable = true;
 
+  services.openssh.settings = {
+    PasswordAuthentication = false;
+    KbdInteractiveAuthentication = false;
+  };
+
+  systemd.tmpfiles.rules = [
+    "d /var/lib/cloudflared 0700 root root -"
+  ];
+
+  systemd.services.cloudflared = {
+    description = "Cloudflare Tunnel";
+    wantedBy = ["multi-user.target"];
+    wants = ["network-online.target"];
+    after = ["network-online.target"];
+
+    serviceConfig = {
+      DynamicUser = true;
+      LoadCredential = "tunnel-token:/var/lib/cloudflared/tunnel-token";
+      ExecStart = "${pkgs.cloudflared}/bin/cloudflared tunnel --no-autoupdate run --token-file %d/tunnel-token";
+      Restart = "always";
+      RestartSec = 5;
+
+      CapabilityBoundingSet = "";
+      LockPersonality = true;
+      NoNewPrivileges = true;
+      PrivateTmp = true;
+      ProtectHome = true;
+      ProtectSystem = "strict";
+    };
+  };
+
   networking.networkmanager.dispatcherScripts = [
     {
       source = pkgs.writeShellScript "magnolia-wifi-fallback" ''
